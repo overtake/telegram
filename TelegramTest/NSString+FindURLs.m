c40a265ded92087b3f0e60df5aa5ac36acf8695f
Psidium <psiidium@gmail.com>
2015-02-19 18:00:39 -0200

    Added detecting of URI

--- TelegramTest/NSString+FindURLs.m
+++ TelegramTest/NSString+FindURLs.m
@@ -64,20 +64,65 @@
         
     }];
     
-    //detect range of URL Schemes
+    //Load the list on the regex
+    NSArray *schemesToFind = @[@"spotify:", @"x-apple.systempreferences://"]; //dumb version
+    NSString *schemeRegexString = @"";
+    for (NSString *scheme in schemesToFind) {
+        schemeRegexString = [schemeRegexString stringByAppendingString:[NSString stringWithFormat:@"(%@.+)|", scheme]];
+    }
+    //remove last character of string |
+    schemeRegexString = [schemeRegexString substringToIndex:schemeRegexString.length-(schemeRegexString.length>0)];
     
-    // Run the following command to have a list of schemes available
-    //System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -dump | egrep "(bindings.*\:$)"|sort
-    
-    
-    //Load the list on the first run to memory
+    //detect occurence of array into string
+    NSRegularExpression *schemeRegex = [NSRegularExpression regularExpressionWithPattern:schemeRegexString options:0 error:&error];
     
     
     //check the contents of messages against the schemes (Regex or other?)
-    
+    NSArray* schemeResult = [schemeRegex matchesInString:self options:0 range:NSMakeRange(0, [self length])];
     //return the range of URL scheme to program
+    NSArray* newResult = [results arrayByAddingObjectsFromArray:schemeResult];
     
-   return [results arrayByAddingObjectsFromArray:userNames];
+    return [newResult arrayByAddingObjectsFromArray:userNames];;
 }
+
+
+
+/*
+ //the following code has been commented because there's no programatic way of determining all the URL shchemes presented on a system, thus forcing the creation of the .plist for handling known URL schemes
+-(NSArray *) listOfURIsOnTheSystem {
+    NSPipe *pipe = [NSPipe pipe];
+    NSFileHandle *file = pipe.fileHandleForReading;
+    
+    NSTask *task = [[NSTask alloc] init];
+    
+    // Run the following command to have a list of schemes available
+    //System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -dump | egrep "(bindings.*\:$)"|sort
+    task.launchPath = @"/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister";
+    task.arguments = @[@"-dump",@"|egrep", @"\"(bindings.*\\:$)\"|sort"];
+    task.standardOutput = pipe;
+    
+    //Run
+    [task launch];
+    
+    NSData *data = [file readDataToEndOfFile];
+    [file closeFile];
+    
+    NSString * output = [[NSString alloc] initWithData: data encoding: NSUTF8StringEncoding];
+    
+    NSError *error = nil;
+    
+    NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:@"(bindings.*\\:$)" options:0 error:&error];
+    
+    if (error) {
+        NSAlert *alert = [[NSAlert alloc] init] ;
+        [alert setMessageText:error.localizedDescription];
+        [alert runModal];
+        return @[];
+    }
+    
+    return @[];
+    
+}
+ */
 
 @end
