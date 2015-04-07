5074d92f602163cdfecacc53767af8338e70b3e2
overtake <overtakeful@gmail.com>
2015-04-07 20:33:02 +0200

    localization changes

--- TelegramTest/RPCRequest.m
+++ TelegramTest/RPCRequest.m
@@ -133,7 +133,7 @@
                 
                 
                 if(self.error.error_code == 502) {
-                    alert(NSLocalizedString(@"App.ConnectionError", nil), NSLocalizedString(@"App.ConnectionErrorDesc", nil));
+                   // alert(NSLocalizedString(@"App.ConnectionError", nil), NSLocalizedString(@"App.ConnectionErrorDesc", nil));
                 }
                 
             }
