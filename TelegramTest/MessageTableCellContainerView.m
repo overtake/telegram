ce2965cec634dcd7b949ccb022af2ebe47853992
overtake <overtakeful@gmail.com>
2015-04-13 21:42:10 +0300

    fixes

--- TelegramTest/MessageTableCellContainerView.m
+++ TelegramTest/MessageTableCellContainerView.m
@@ -812,7 +812,7 @@ static BOOL dragAction = NO;
         
         [_replyContainer setReplyObject:item.replyObject];
         
-        [_replyContainer setFrame:NSMakeRect(NSMinX(_replyContainer.frame), NSHeight(_containerView.frame) + 15, item.blockSize.width , item.replyObject.containerHeight)];
+        [_replyContainer setFrame:NSMakeRect(NSMinX(_replyContainer.frame), NSHeight(_containerView.frame) + 15, item.blockWidth - 60 , item.replyObject.containerHeight)];
         
         
     } else {
