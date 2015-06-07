e4d0978cf926ce864f7cbe5d61332b109efb27c3
overtake <overtakeful@gmail.com>
2015-06-07 19:07:45 +0300

    commit

---
+++ TelegramTest/TGAudioPlayerWindow.h
@@ -0,0 +1,20 @@
+//
+//  TGAudioPlayerWindow.h
+//  Telegram
+//
+//  Created by keepcoder on 01.06.15.
+//  Copyright (c) 2015 keepcoder. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import "MessageTableItemAudioDocument.h"
+@interface TGAudioPlayerWindow : NSPanel
+
+@property (nonatomic,strong,readonly) MessageTableItemAudioDocument *currentItem;
+
++(void)show:(TL_conversation *)conversation;
+
+
++(void)setCurrentItem:(MessageTableItemAudioDocument *)audioItem;
+
+@end
