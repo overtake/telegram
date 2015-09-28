24ff8680167dfa01c1ca34a405c19c249b5e1b9f
overtake <overtakeful@gmail.com>
2015-09-28 19:11:55 +0300

    commit

---
+++ TelegramTest/TGModernUserViewController.h
@@ -0,0 +1,13 @@
+//
+//  TGModernUserViewController.h
+//  Telegram
+//
+//  Created by keepcoder on 28.09.15.
+//  Copyright (c) 2015 keepcoder. All rights reserved.
+//
+
+#import "ComposeViewController.h"
+
+@interface TGModernUserViewController : ComposeViewController
+
+@end
---
+++ TelegramTest/TGModernUserViewController.m
@@ -0,0 +1,19 @@
+//
+//  TGModernUserViewController.m
+//  Telegram
+//
+//  Created by keepcoder on 28.09.15.
+//  Copyright (c) 2015 keepcoder. All rights reserved.
+//
+
+#import "TGModernUserViewController.h"
+
+@implementation TGModernUserViewController
+
+-(void)loadView {
+    [super loadView];
+    
+    
+}
+
+@end
