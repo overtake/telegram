0105a4acd85bb9c28885a14aaea5ec27d536ab5e
overtake <overtakeful@gmail.com>
2015-10-25 20:02:38 +0300

    fixes

--- TelegramTest/TGPVDocumentsBehavior.m
+++ TelegramTest/TGPVDocumentsBehavior.m
@@ -135,9 +135,9 @@
             
             TL_messageMediaDocument *media = (TL_messageMediaDocument *) [(TL_localMessage *)obj.media media];
             
-           TL_documentAttributeImageSize *size = (TL_documentAttributeImageSize *) [[media document] attributeWithClass:[TL_documentAttributeImageSize class]];
-            
-            if(size) {
+            if([media.document.mime_type hasPrefix:@"image"]) {
+                TL_documentAttributeImageSize *size = (TL_documentAttributeImageSize *) [[media document] attributeWithClass:[TL_documentAttributeImageSize class]];
+                
                 TGPVDocumentObject *imgObj = [[TGPVDocumentObject alloc] initWithMessage:obj.media placeholder:nil];
                 
                 if(size) {
@@ -151,7 +151,6 @@
                 TGPhotoViewerItem *item = [[TGPhotoViewerItem alloc] initWithImageObject:imgObj previewObject:obj];
                 [converted addObject:item];
             }
-            
         }
         
     }];
