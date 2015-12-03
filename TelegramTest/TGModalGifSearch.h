aa23e67f31accb87e50b13f9660b9fced020906b
overtake <overtakeful@gmail.com>
2015-12-03 14:22:05 +0300

    gif

---
+++ TelegramTest/TGModalGifSearch.h
@@ -0,0 +1,13 @@
+//
+//  TGModalGifSearch.h
+//  Telegram
+//
+//  Created by keepcoder on 03/12/15.
+//  Copyright © 2015 keepcoder. All rights reserved.
+//
+
+#import "TGModalView.h"
+
+@interface TGModalGifSearch : TGModalView
+
+@end
---
+++ TelegramTest/TGModalGifSearch.m
@@ -0,0 +1,19 @@
+//
+//  TGModalGifSearch.m
+//  Telegram
+//
+//  Created by keepcoder on 03/12/15.
+//  Copyright © 2015 keepcoder. All rights reserved.
+//
+
+#import "TGModalGifSearch.h"
+
+@implementation TGModalGifSearch
+
+- (void)drawRect:(NSRect)dirtyRect {
+    [super drawRect:dirtyRect];
+    
+    // Drawing code here.
+}
+
+@end
