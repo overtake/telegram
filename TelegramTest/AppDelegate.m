d143771c1cf98a181820c11e522fa72177276306
Tay Yang Shun <tay.yang.shun@gmail.com>
2017-01-15 15:11:37 +0800

    Improve emoji autocomplete UX

--- TelegramTest/AppDelegate.m
+++ TelegramTest/AppDelegate.m
@@ -527,7 +527,10 @@ void exceptionHandler(NSException * exception)
         }
         
         if(incomingEvent.keyCode == 125 || incomingEvent.keyCode == 126) {
-            if(((incomingEvent.modifierFlags & (NSAlternateKeyMask)) > 0 || (incomingEvent.modifierFlags & (NSControlKeyMask)) > 0 || (incomingEvent.modifierFlags & (NSShiftKeyMask)) > 0 )&& incomingEvent.modifierFlags != 10617090) {
+            if(((incomingEvent.modifierFlags & (NSAlternateKeyMask)) > 0 || 
+                (incomingEvent.modifierFlags & (NSControlKeyMask)) > 0 || 
+                (incomingEvent.modifierFlags & (NSShiftKeyMask)) > 0)
+                && incomingEvent.modifierFlags != 10617090) {
                 BOOL result = YES;
                 
 //                if([responder isKindOfClass:[NSTextField class]]) {
@@ -738,14 +741,7 @@ void exceptionHandler(NSException * exception)
                 [TMViewController becomePasslock];
             }
         }
-        
-        if(result.keyCode == 48) {
-          //  NSTextView *textView = responder;
-            
-            
-            return [[NSEvent alloc]init];
-        }
-        
+
         if(isEnterAccess(result)) {
             if([appWindow().navigationController.currentController proccessEnterAction]) {
                 return [[NSEvent alloc] init];
